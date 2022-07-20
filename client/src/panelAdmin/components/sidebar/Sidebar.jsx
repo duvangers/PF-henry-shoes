fa73ddaf7d04e07eabfc2dbc6ec0e0537974dbbb
Dv-del <alexis1614@live.com.ar>
2022-07-20 17:53:12 -0300

    main

--- client/src/panelAdmin/components/sidebar/Sidebar.jsx
+++ client/src/panelAdmin/components/sidebar/Sidebar.jsx
@@ -1,26 +1,26 @@
-import "./sidebar.scss";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
-import StoreIcon from "@mui/icons-material/Store";
-import InsertChartIcon from "@mui/icons-material/InsertChart";
-import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
-import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import { Link } from "react-router-dom";
+import './sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+import StoreIcon from '@mui/icons-material/Store'
+import InsertChartIcon from '@mui/icons-material/InsertChart'
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined'
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import { Link } from 'react-router-dom'
 // import { DarkModeContext } from "../../context/darkModeContext";
 // import { useContext } from "react";
-import logotoro from "../../../logotoro.png";
+import logotoro from '../../../logotoro.png'
 const Sidebar = () => {
   // const { dispatch } = useContext(DarkModeContext);
   return (
     <div className="sidebar">
       <div className="top">
-        <Link to="/admin" style={{ textDecoration: "none" }}>
+        <Link to="/admin" style={{ textDecoration: 'none' }}>
           <span className="logo">
             {/* <img
               src={logotoro}
@@ -38,26 +38,26 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <p className="title">PRINCIPAL</p>
-          <Link to="/admin" style={{ textDecoration: "none" }}>
+          <Link to="/admin" style={{ textDecoration: 'none' }}>
             <li>
               <DashboardIcon className="icon" />
               <span>Dashboard</span>
             </li>
           </Link>
           <p className="title">LISTAS</p>
-          <Link to="/admin/clients" style={{ textDecoration: "none" }}>
+          <Link to="/admin/clients" style={{ textDecoration: 'none' }}>
             <li>
               <PersonOutlineIcon className="icon" />
               <span>Clientes</span>
             </li>
           </Link>
-          <Link to="/admin/products" style={{ textDecoration: "none" }}>
+          <Link to="/admin/products" style={{ textDecoration: 'none' }}>
             <li>
               <StoreIcon className="icon" />
               <span>Productos</span>
             </li>
           </Link>
-          <Link to="/admin/orders" style={{ textDecoration: "none" }}>
+          <Link to="/admin/orders" style={{ textDecoration: 'none' }}>
             <li>
               <CreditCardIcon className="icon" />
               <span>Ordenes</span>
@@ -95,13 +95,13 @@ const Sidebar = () => {
             </li>
           </Link> */}
           <p className="title">USUARIO</p>
-          <Link to="/admin/profile" style={{ textDecoration: "none" }}>
+          <Link to="/admin/profile" style={{ textDecoration: 'none' }}>
             <li>
               <AccountCircleOutlinedIcon className="icon" />
               <span>Perfil</span>
             </li>
           </Link>
-          {/* <Link to="/admin/logout" style={{ textDecoration: "none" }}>
+          {/* <Link to="/admin/" style={{ textDecoration: "none" }}>
             <li>
               <ExitToAppIcon className="icon" />
               <span>Salir</span>
@@ -120,7 +120,7 @@ const Sidebar = () => {
         ></div>
       </div> */}
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
