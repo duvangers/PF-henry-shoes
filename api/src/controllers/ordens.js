0bde68386dd50bbc94295b7d6b7356441fa7d2ed
alemosmusi <alemosmusi@gmail.com>
2022-07-22 13:20:13 -0300

    arreglo de metodo de pago

--- api/src/controllers/ordens.js
+++ api/src/controllers/ordens.js
@@ -8,119 +8,95 @@ const {
 } = require("../db");
 
 const getOrdens = async (req, res) => {
-  try {
-    const response = await modelOrdens.findAll(
-      {
-        include: [modelUsers, modelProducts],
-      },
-      { raw: true }
-    );
-
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(500).send({ msg: "Error interno del servidor.", error });
-  }
-};
+    try {
+        const response = await modelOrdens.findAll(
+          {
+            include: [modelUsers, modelProducts],
+          },
+          { raw: true }
+        );
+    
+        res.status(200).json(response);
+      } catch (error) {
+        res.status(500).send({ msg: "Error interno del servidor.", error });
+      }
+    };
 
 const createOrden = async (req, res) => {
     const { userId: id } = req.params
-    const { details, price_total, amount_total, metodopago } = req.body
-
-  const updateStock = (arraySizes, size, amount) => {
-    const obj = arraySizes.find((obj) => obj.size === size);
-
-    if (!obj) return false;
-    if (obj.stock === 0) return false;
-    if (obj.stock < amount) return false;
-
-    const index = arraySizes.indexOf(obj);
-    arraySizes[index].stock -= amount;
-
-    return arraySizes;
-  };
-
-  try {
-    const user = await modelUsers.findByPk(id);
-    if (!user)
-      return res
-        .status(400)
-        .send({ msg: `El usuario ${id} no existe en la base de datos.` });
-
-    const products = await Promise.all(
-      details.map((obj) => {
-        return modelProducts.findByPk(obj.productID);
-      })
-    );
-    if (!products) {
-      return res.status(400).send({
-        msg: "Uno de los productos no están registrados en la base de datos.",
-      });
-    }
-
-    let array = [];
-
-    for (let i = 0; i < details.length; i++) {
-      const { size_range } = products[i];
-
-      const modSizes = updateStock(
-        size_range,
-        details[i].size,
-        details[i].amount
-      );
-      if (!modSizes) {
-        return res.status(400).send({
-          msg: `El producto ${products[i].id} de la talla ${details[i].size} no cuenta con el stock necesario (o no existe la talla) para generar esta orden.`,
-        });
-      }
+    const { details, price_total, amount_total, metodopago } = req.body;
+  
+    const updateStock = (arraySizes, size, amount) => {
+      const obj = arraySizes.find((obj) => obj.size === size);
+  
+      if (!obj) return false;
+      if (obj.stock === 0) return false;
+      if (obj.stock < amount) return false;
+  
+      const index = arraySizes.indexOf(obj);
+      arraySizes[index].stock -= amount;
+  
+      return arraySizes;
+    };
 
     try {
-        const user = await modelUsers.findByPk(id)
-        if (!user) return res.status(400).send({msg: `El usuario ${id} no existe en la base de datos.`})
-
-        const products = await Promise.all(details.map(obj => {
-            return modelProducts.findByPk(obj.productID)
-        }))
+        const user = await modelUsers.findByPk(id);
+        if (!user)
+          return res
+            .status(400)
+            .send({ msg: `El usuario ${id} no existe en la base de datos.` });
+    
+        const products = await Promise.all(
+          details.map((obj) => {
+            return modelProducts.findByPk(obj.productID);
+          })
+        );
         if (!products) {
-            return res.status(400).send({msg: 'Uno de los productos no están registrados en la base de datos.'})
+          return res.status(400).send({
+            msg: "Uno de los productos no están registrados en la base de datos.",
+          });
         }
-
         let array = []
-
         for (let i = 0; i < details.length; i++) {
-            const { size_range } = products[i]
+            const { size_range } = products[i];
 
-            const modSizes = updateStock(size_range, details[i].size, details[i].amount)
-            if (!modSizes) {
-                return res.status(400).send({msg: `El producto ${products[i].id} de la talla ${details[i].size} no cuenta con el stock necesario (o no existe la talla) para generar esta orden.`})
-            }
-            
-            array.push(modSizes)
-        }
-        
-        array.forEach(async (obj, index) => {
-            await modelProducts.update({size_range: obj}, {where: {id: products[index].id}})
-        })
-
-        const order = await modelOrdens.create({
-            amount_total,
-            price_total,
-            details,
-            metodopago
-        })
-
-        let relProduct = details.map(obj => obj.productID)
-        relProduct = [... new Set(relProduct)]
-
-        order.setUser(id)
-        order.setProducts(relProduct)
-        
-        res.status(200).send({msg: 'Orden creada con éxito.'})
-    } catch (error) {
-        res.status(500).send({msg: 'Error interno del servidor.', error})
-
-      array.push(modSizes);
-
+            const modSizes = updateStock(
+                size_range,
+                details[i].size,
+                details[i].amount
+              );
+              if (!modSizes) {
+                return res.status(400).send({
+                  msg: `El producto ${products[i].id} de la talla ${details[i].size} no cuenta con el stock necesario (o no existe la talla) para generar esta orden.`,
+                });
+              }
+              array.push(modSizes);
     }
+        
+    array.forEach(async (obj, index) => {
+        await modelProducts.update(
+          { size_range: obj },
+          { where: { id: products[index].id } }
+        );
+      });
+
+      const order = await modelOrdens.create({
+        amount_total,
+        price_total,
+        details,
+        metodopago
+      });
+      let relProduct = details.map((obj) => obj.productID);
+      relProduct = [...new Set(relProduct)];
+
+      order.setUser(id);
+      order.setProducts(relProduct);
+        
+      res.status(200).send({ msg: "Orden creada con éxito." });
+    } catch (error) {
+      res.status(500).send({ msg: "Error interno del servidor.", error });
+    }
+  };
 
     array.forEach(async (obj, index) => {
       await modelProducts.update(
@@ -128,25 +104,7 @@ const createOrden = async (req, res) => {
         { where: { id: products[index].id } }
       );
     });
-
-    const order = await modelOrdens.create({
-      amount_total,
-      price_total,
-      details,
-    });
-
-    let relProduct = details.map((obj) => obj.productID);
-    relProduct = [...new Set(relProduct)];
-
-    order.setUser(id);
-    order.setProducts(relProduct);
-
-    res.status(200).send({ msg: "Orden creada con éxito." });
-  } catch (error) {
-    res.status(500).send({ msg: "Error interno del servidor.", error });
-  }
-};
-
+    
 const updateOrden = async (req, res) => {
   const { ordenId: id } = req.params;
   const { state } = req.body;
