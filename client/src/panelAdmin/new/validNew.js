68c4e055a2be34be3b1c1ea7b33008733498bdd2
diegop1zarro <diego.cab.1016@gmail.com>
2022-07-22 05:39:38 -0300

    New Products diego

--- client/src/panelAdmin/new/validNew.js
+++ client/src/panelAdmin/new/validNew.js
@@ -1,25 +1,28 @@
 import * as Yup from "yup";
 
-export const validSchema = Yup.object().shape({
-    name: Yup.string().required("You must enter a valid and unique name"),
-    brandID: Yup.string().required("you must select a shoe brand, if the brand is not found you can create it"),
-    description: Yup.string().required("put a brief description"),
-    price: Yup.number().min(1).required("you must put a price"),
-   material: Yup.string().required("you must place the material from which it is made"),
-   released: Yup.date().required("you must put the release date"),
-   designer: Yup.string().required("Enter the name of the designer"),
-   details: Yup.string().required("put the main details"),
-   shoe_condition: Yup.string().required("place the condition in which the product is found"),
-   categories: Yup.array().of(Yup.string().required("place which category it belongs to")),
-   colorID: Yup.string().required("You must choose a color, if you don't find the right one you can create one"),
-   genderID: Yup.string().required("you must choose the gender of the size, if you do not find the correct one you can create one"),
-  //  size_range:Yup.required('debes elegir los talles disponibles con sus stock disponible'),
-  // size_range:Yup.array().required('debes elegir un talle y su stock')
-
-
+ Yup.addMethod(Yup.array, 'unique', function(categories, mapper = a => a) {
+  return this.test('unique', categories, function(list) {
+      return list.length  === new Set(list.map(mapper)).size;
   });
+});
+ export const validSchema = Yup.object().shape({
+    name: Yup.string().min(4,"mínimo 4 caracteres").required("Debe ingresar un nombre válido y único"),
+    brandID: Yup.string().required("debe seleccionar una marca de zapatos, si no se encuentra la marca puede crearla(luego apretar el boton de recargar)"),
+    description: Yup.string().min(10,"mínimo 10 caracteres ").required("poner una breve descripción"),
+    price: Yup.number().min(1).required("debes poner precio"),
+   material: Yup.string().required("debes colocar el material del que está hecho"),
+   released: Yup.date().required("debes poner la fecha de lanzamiento"),
+   designer: Yup.string().required("Introduce el nombre del diseñador."),
+   details: Yup.string().min(5,"mínimo 5 caracteres").required("poner los detalles principales"),
+   shoe_condition: Yup.string().required("colocar el estado en que se encuentra el producto"),
+   categories: Yup.array().of(Yup.string().required("colocar a que categoria pertenece, si no se encuentra la categoria puede crearla(luego apretar el boton de recargar)")).unique('no debes tener categorias repetidas'),
+   colorID: Yup.string().required("Debes elegir un color."),
+   genderID: Yup.string().required("debes elegir el genero de la talla"),
+  //  img:Yup.required('is requerid url of img'),
 
+});
 
+  
 export const initialValue ={
   name: "",
   nickname:"",
@@ -35,7 +38,7 @@ export const initialValue ={
   designer: "",
   details: "",
   shoe_condition: "",
-  // rating: 0,
-  categories: [],
+  rating: 0,
+  categories: [''],
 
 }
